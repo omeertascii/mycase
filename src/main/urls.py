efc10dc236abd1646c1487ba42e89622297d4a1a
TASH <tasci_omer@windowslive.com>
2024-08-21 15:39:48 +0300

    update: user app urls

--- src/main/urls.py
+++ src/main/urls.py
@@ -1,14 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import ExampleView
-
-router = DefaultRouter()
-router.register(r'workgroups', ExampleView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('rest_framework.urls')),
-
-    path('', include(router.urls)),
+    path('user/', include('user.urls')),
     ]
